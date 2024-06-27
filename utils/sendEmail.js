821e7f800ef7d63b8e11fa55c7be68aae1a3aedd
Nargis Khatun <itxnargiskhatun@gmail.com>
2024-06-27 18:51:45 +0530

    Fix and update project files

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,26 +1,29 @@
 const nodeMailer = require("nodemailer");
 
 const sendEmail = async (options) => {
+    try {
+        const transporter = nodeMailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: Number(process.env.SMTP_PORT),
+            secure: true, // Use SSL/TLS
+            auth: {
+                user: process.env.SMTP_MAIL,
+                pass: process.env.SMTP_PASSWORD, // Use the app-specific password here
+            }
+        });
 
-    const transporter = nodeMailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        secure: true, // Use SSL/TLS
-        auth: {
-            user: process.env.SMTP_MAIL,
-            pass: process.env.SMTP_PASSWORD, // Use the app-specific password here
-        }
-    });
+        const mailOptions = {
+            from: process.env.SMTP_MAIL,
+            to: options.email,
+            subject: options.subject,
+            text: options.message,
+        };
 
-    const mailOptions = {
-        from: process.env.SMTP_MAIL,
-        to: options.email,
-        subject: options.subject,
-        text: options.message,
-    };
-
-    await transporter.sendMail(mailOptions);
-
+        await transporter.sendMail(mailOptions);
+    } catch (error) {
+        console.error(`Error sending email: ${error.message}`);
+        throw new Error('Email could not be sent');
+    }
 };
 
 module.exports = sendEmail;
