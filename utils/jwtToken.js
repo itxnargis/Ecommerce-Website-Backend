27f4230d7019e479f0774313dac0899119b66d80
Nargis Khatun <itxnargiskhatun@gmail.com>
2024-07-02 23:50:11 +0530

    Fix: Authentication in order and products

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,15 +1,7 @@
 const sendToken = (user, statusCode, res, message = '') => {
     const token = user.getJWTToken();
-    // const options = {
-    //     expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
-    //     httpOnly: true,
-    // };
-    const options = {
-        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-        httpOnly: true,
-    };
 
-    res.status(statusCode).cookie("token", token, options).json({
+    res.status(statusCode).json({
         success: true,
         user,
         token,
