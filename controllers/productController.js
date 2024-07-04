9af21fd5e950bd8dcf7921106f5af27d9841cf72
Nargis Khatun <itxnargiskhatun@gmail.com>
2024-07-04 22:43:23 +0530

    Fix - modify code

--- controllers/productController.js
+++ controllers/productController.js
@@ -50,6 +50,8 @@ exports.getAllProducts = catchAsyncErrors(async (req, res, next) => {
   const products = await apiFeature.query;
   const filteredProductsCount = products.length;
   apiFeature.pagination(resultPerPage);
+  products = await apiFeature.query.clone();
+
   res.status(200).json({
     success: true,
     products,
