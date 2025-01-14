4f0b1110225d6a1929629fba8d3a47919d329286
itxnargis <itxnargiskhatun@gmail.com>
2025-01-14 19:07:35 +0530

    fix error fot forgot password

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,33 +1,30 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
-    try {
-        console.log("Creating transporter");
-        const transporter = nodemailer.createTransport({
-            host: process.env.SMTP_HOST,
-            port: Number(process.env.SMTP_PORT),
-            secure: true,
-            auth: {
-                user: process.env.SMTP_MAIL,
-                pass: process.env.SMTP_PASSWORD,
-            },
-        });
-        console.log("Creating transporter success");
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: Number(process.env.SMTP_PORT),
+      service: process.env.SMTP_SERVICE,
+      auth: {
+        user: process.env.SMTP_MAIL,
+        pass: process.env.SMTP_PASSWORD,
+      },
+    });
 
-        const mailOptions = {
-            from: process.env.SMTP_MAIL,
-            to: options.email,
-            subject: options.subject,
-            text: options.message,
-        };
-        console.log(`Sending message to ${options.email} `)
+    const mailOptions = {
+      from: process.env.SMTP_MAIL,
+      to: options.email,
+      subject: options.subject,
+      text: options.message,
+    };
 
-        await transporter.sendMail(mailOptions);
-        console.log("Email sent successfully")
-    } catch (error) {
-        console.error(`Error sending email: ${error.message}`);
-        throw new Error('Email could not be sent');
-    }
+    await transporter.sendMail(mailOptions);
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.error(`Error sending email: ${error.message}`);
+    throw new Error('Email could not be sent');
+  }
 };
 
 module.exports = sendEmail;
